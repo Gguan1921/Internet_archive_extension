beb075a7a480fb0d7ca0220a9688d384a2a009f7
Gguan1921 <56061173+Gguan1921@users.noreply.github.com>
2022-03-06 16:36:50 -0800

    Stuck on feting summary page

--- wayback_check.js
+++ wayback_check.js
@@ -18,22 +18,48 @@ async function check_avil(theUrl)
         const message = `Failed to fetch status: ${response.status}`;
         throw new Error (message);
     }
-
     const result = await response.json();
+    if (Object.keys(result["archived_snapshots"]).length === 0)
+    {
+        return false;
+    }
 
     return result["archived_snapshots"]["closest"]["available"];
+}
+// fetch the earliest url
+async function get_earliest_url(theUrl)
+{
+    var rUrl = "https://web.archive.org/details/" + theUrl;
+    const response = await fetch(rUrl);
+    if (!response.ok){
+        const message = `Failed to fetch summary page: ${response.status}`;
+        throw new Error (message);
+    }
+    const result = await response.json();
+
+    return result;
 }
 
 function main()
 {
     var cUrl = get_ori_url();
     var avil;
+    var summary_page;
     (async function() {
         try{
             avil = await check_avil(cUrl);
-            console.log (avil);
+            if (!avil) return;
         } catch (e) {
             console.log (e);
+            return;
+        }
+
+        try {
+            summary_page = await get_earliest_url(cUrl);
+            console.log(summary_page);
+        } catch(e) {
+            console.log (e);
+            return;
         }
     })();
 }
